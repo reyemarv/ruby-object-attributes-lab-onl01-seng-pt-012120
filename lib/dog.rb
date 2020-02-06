926241f0ba32d077e8d0efdaec0f23da8b321e00
Marvin Reyes <reyemarv@gmail.com>
2020-02-06 15:30:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name 
+  end 
+  
+  def breed=(breed_new)
+    @breed = breed_new
+  end
+  
+  def breed 
+    @breed
+  end 
+  
+end 
