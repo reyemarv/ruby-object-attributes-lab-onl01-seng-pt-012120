1f2e0bd3d0e7c6a7a56b565f0d28cdbcce89e540
Marvin Reyes <reyemarv@gmail.com>
2020-02-06 15:14:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,15 +12,4 @@ class Dog
     @name
   end 
   
-  def initialize(breed)
-    @breed = breed 
-  end
-  
-  def breed=(new_breed)
-    @breed = new_breed
-  end 
-  
-  def breed
-    @breed
-  end
 end 
